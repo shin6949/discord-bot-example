575c48a7e62e7dc7c5bcdc6fa94a7bc273640e6a
cocoblue <cocoblue@kakao.com>
2022-02-22 20:30:40 +0900

    불필요 코드 제거

--- index.mjs
+++ index.mjs
@@ -8,10 +8,7 @@ const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 // Slash Command 추가
 registerCommands(process.env.DISCORD_BOT_TOKEN, process.env.CLIENT_ID, process.env.TO_REGISTER_GUILD);
 
-client.once('ready', () => {
-    console.log('Ready to receive interaction');
-});
-
+// 메시지를 받으면 호출되는 함수
 client.on('interactionCreate', async interaction => {
     // Original: https://discordjs.guide/interactions/replying-to-slash-commands.html#receiving-interactions
     if (!interaction.isCommand()) return;
